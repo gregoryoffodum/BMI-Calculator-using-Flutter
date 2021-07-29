9d15b2ac0278533cae08a72413bf235be13924e0
Gregory Okechukwu Offodum <gregoryoffodum@gmail.com>
2021-07-29 03:04:51 +0100

    first commit

---
+++ lib/components/icon_content.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+
+
+
+class IconContent extends StatelessWidget {
+
+  IconContent({this.icon, this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon,
+          size: 20,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(label,
+          style: kLabelTextStyle,
+          ),
+      ],
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+
+
+const kLabelTextStyle = TextStyle(
+    fontSize: 10, color: Color(0xFF8D8E98)
+);
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFF111328);
+const kInactiveCardColour = Color(0xFF1D1E33);
+const kBottomContainerColour = Color(0xFFEB15555);
+const kNumberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.bold
+);
+
+const kTitleTextStyle = TextStyle(
+    fontSize: 50,
+    fontWeight: FontWeight.bold
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 45,
+  fontWeight: FontWeight.bold
+);
+
+const kBMITextStyle = TextStyle(
+    fontSize: 100,
+    fontWeight: FontWeight.bold
+);
+
+const kBodyTextStyle = TextStyle(
+    fontSize: 22,
+);
